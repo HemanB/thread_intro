9ad6f9f9741cb0120a205dfe9bc6e55ae342cdb0
Heman Burre <hsb26@duke.edu>
2024-11-18 15:52:42 -0500

    Completed thread_intro.

--- thread3.c
+++ thread3.c
@@ -20,13 +20,17 @@ void print_args(args_t *args) {
 }
 
 void *thread_double(void *argin) {
-
-
   // fill in this function to have the thread update its range of array entries
   // each entry in the array should be multiplied by 2.
   // also call print_thread_info
+  args_t *args = (args_t *)argin;
+  print_thread_info();
+  print_args(args);
+  for (int i = args->start; i < args->end; i++) {
+    array[i] *= 2;
+  }
+  printf("%ld done\n", Thread_gettid());
   return NULL;
-
 }
 
 int main (int argc, char **argv)
@@ -44,12 +48,19 @@ int main (int argc, char **argv)
     sum += array[i];
   }
   printf("main: before %d\n",sum);
-
   // fill in this portion to create 4 threads that operate on 1/4 of the array
   // you need to setup the arg structure appriately
   // main should use join to wait for all four threads to finish before executing the finish up code
-
-
+  for (int i = 0; i < NUM_THREADS; i++) {
+    args[i].id = i;
+    args[i].start = i * (SIZE / NUM_THREADS);
+    args[i].end = (i + 1) * (SIZE / NUM_THREADS);
+    Pthread_create(&threads[i], NULL, thread_double, (void *)&args[i]);
+  }
+  // wait step
+  for (int i = 0; i < NUM_THREADS; i++) {
+    Pthread_join(threads[i], NULL);
+  }
   // main finish up code.
   sum = 0;
   for (int i =0; i < SIZE; i++) {
